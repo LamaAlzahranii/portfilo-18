de7714b44d709369b505c17c66e1dcf0933d2b96
LamaAlzahranii <lamaahmed.alzahrani@gmail.com>
2024-06-25 02:19:04 +0300

    add mobile config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{html,ts}",
-    ],
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [""],
+    },
+    screens: {
+      xl: { max: "5000px" },
+      lg: { max: "1919px" },
+      md: { max: "1279px" },
+      sm: { max: "959px" },
+      xs: { max: "599px" },
+    },
   },
   plugins: [],
-}
-
+};
